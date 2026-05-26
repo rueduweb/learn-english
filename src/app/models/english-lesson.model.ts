d4eef61643e9626b870587559c1a6d12a6a6dd3a
rueduweb <rhu.sab@outlook.fr>
2026-05-26 09:30:06 +0200

    add english lesson model

---
+++ src/app/models/english-lesson.model.ts
@@ -0,0 +1,18 @@
+export type EnglishInfoBase = {
+  title: string;
+  description: string;
+  category: string;
+  day: Date;
+  duration: number;
+  current_status: string;
+};
+
+export type EnglishLesson = EnglishInfoBase & {
+  coach: string;
+  groupLesson: boolean;
+};
+
+export type EnglishExercice = EnglishInfoBase & {
+  score: string;
+  comment: string | undefined;
+};
