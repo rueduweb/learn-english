856fd2dde02bc2ce3e30cd02e996a8f719a8c294
rueduweb <rhu.sab@outlook.fr>
2026-06-13 12:12:14 +0200

    create english exercices service

--- src/app/feature-english-exercices/services/english-exercices.service.ts
+++ src/app/feature-english-exercices/services/english-exercices.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { EnglishExercice } from '../../shared/models/english-item.model';
 
 @Injectable({
@@ -6,4 +8,23 @@ import { EnglishExercice } from '../../shared/models/english-item.model';
 })
 export class EnglishExercicesService {
   private readonly baseURL = 'http://localhost:3000/englishexercices';
+
+  http = inject(HttpClient);
+
+  getEnglishExercices(): Observable<EnglishExercice[]> {
+      return this.http.get<EnglishExercice[]>(`${this.baseURL}`);
+    }
+
+  createEnglishExercice(lesson: EnglishExercice): Observable<EnglishExercice> {
+    return this.http.post<EnglishExercice>(this.baseURL, lesson);
+  }
+
+  updateEnglishExercice(id: string, lesson: EnglishExercice): Observable<EnglishExercice> {
+    return this.http.put<EnglishExercice>(`${this.baseURL}/${id}`, lesson);
+  }
+
+  deleteEnglishExercice(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseURL}/${id}`);
+  }
+
 }
