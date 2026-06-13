33c5dd56a388464c74da6dc3461fa74e95f070e2
rueduweb <rhu.sab@outlook.fr>
2026-06-13 14:37:59 +0200

    add english exercise list component and its route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EnglishLessonsListComponent } from '../app/feature-english-lessons/english-lessons-list/english-lessons-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EnglishExercisesListComponent } from './feature-english-exercises/english-exercises-list/english-exercises-list.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'lessons', component: EnglishLessonsListComponent },
+  { path: 'exercises', component: EnglishExercisesListComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
---
+++ src/app/feature-english-exercises/english-exercises-list/english-exercises-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSortModule } from '@angular/material/sort';
+
+import { EnglishExercise } from '../../shared/models/english-item.model';
+import { EnglishExercisesStore } from '../store/english-exercises.store';
+import { EnglishExercisesService } from '../services/english-exercises.service';
+
+@Component({
+  selector: 'app-english-exercises-list',
+  imports: [
+    MatIconModule, MatTableModule,MatButtonModule, MatPaginatorModule,
+    MatFormFieldModule, MatInputModule, MatSortModule,
+
+  ],
+  templateUrl: './english-exercises-list.component.html',
+  styleUrl: './english-exercises-list.component.css',
+})
+export class EnglishExercisesListComponent implements OnInit{
+  store = inject(EnglishExercisesStore);
+  exerciseService = inject(EnglishExercisesService);
+  nbExercises = this.store.totalExercises;
+
+  dataSource = new MatTableDataSource<EnglishExercise>();
+
+  displayedColumns: string[] = ['Id','title', 'description', 'category', 'duration', 'current_status', 'score', 'comment'];
+
+  exercisesDataArray: EnglishExercise[] = [];
+
+  ngOnInit() {
+    this.loadEnglishExercises().then(() => {
+      this.exercisesDataArray = this.store.Exercises();
+      this.dataSource = new MatTableDataSource<EnglishExercise>(this.exercisesDataArray);
+    })
+  }
+
+  async loadEnglishExercises() {
+    await this.store.loadExercises()
+  }
+}
